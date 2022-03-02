449e8a3166101e8b57fbebbf8c5a6f677c1930e0
Sergey Semenov <s3menov.semenow@yandex.ru>
2022-03-02 19:32:33 +0300

    added goodList, good

---
+++ src/components/GoodsList.js
@@ -0,0 +1,26 @@
+import Good from "./Good";
+
+const GoodsList = () => {
+    return (
+        <>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+                <Good/>
+            </div>
+        </>
+    );
+  }
+  
+  export default GoodsList;
+  
