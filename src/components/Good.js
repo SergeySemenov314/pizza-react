5dbba272cd59a9fb7c2d6ac4052d36169aae478e
@SergeySemenov314 <s3menov.semenow@yandex.ru>
2022-03-04 20:01:45 +0300

    added goodsArr

--- src/components/Good.js
+++ src/components/Good.js
@@ -1,23 +1,31 @@
-const Good = () => {
+import {useState} from 'react';
+
+const Good = ({addToCart, img, title, doughThickness, sizes, price}) => {
+
+    const [goodObj, setGoodObj] = useState();
+
+
     return (
         <div className="pizza-block">
             <img className="pizza-block__image"
-                src="https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg"
-                alt="Pizza" />
-            <h4 className="pizza-block__title">Чизбургер-пицца</h4>
+                src={img}
+                alt={title} 
+                />
+            <h4 className="pizza-block__title">{title}</h4>
             <div className="pizza-block__selector">
                 <ul>
-                    <li className="active">тонкое</li>
-                    <li>традиционное</li>
+                    {doughThickness.map((item, index) =>
+                        <li className={index === 0 ? 'active'  : ''}>{item}</li>
+                    )}
                 </ul>
                 <ul>
-                    <li className="active">26 см.</li>
-                    <li>30 см.</li>
-                    <li>40 см.</li>
+                    {sizes.map((item, index) =>
+                        <li className={index === 0 ? 'active'  : ''}>{item} см.</li>
+                    )}
                 </ul>
             </div>
             <div className="pizza-block__bottom">
-                <div className="pizza-block__price">от 395 ₽</div>
+                <div className="pizza-block__price">от {price} ₽</div>
                 <div className="button button--outline button--add">
                     <svg width="12" height="12" viewBox="0 0 12 12" fill="none"
                         xmlns="http://www.w3.org/2000/svg">
