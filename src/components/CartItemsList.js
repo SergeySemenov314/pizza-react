386b17f183dbcfb8e7fc2ffb90229082b173a331
Sergey Semenov <s3menov.semenow@yandex.ru>
2022-04-17 23:44:41 +0300

    added CartEmpty

--- src/components/CartItemsList.js
+++ src/components/CartItemsList.js
@@ -1,20 +1,20 @@
-import { useDispatch, useSelector } from "react-redux"
-import CartItem from "./CartItem"
+import { useDispatch } from "react-redux";
+import CartItem from "./CartItem";
 
-const CartItemsList = () => {
-    const {cartGoods} = useSelector(({cartReducer}) => cartReducer)
-    
-    const dispatch = useDispatch()
-
-    // console.log(cartGoods)
+const CartItemsList = ({ cartGoods }) => {
+    const dispatch = useDispatch();
 
     return (
         <div className="content__items">
             {cartGoods.map((cartGood, index) => (
-                <CartItem key = {cartGood.title + cartGood.dough + cartGood.sizes} cartGood={cartGood} dispatch={dispatch} />
+                <CartItem
+                    key={cartGood.title + cartGood.dough + cartGood.sizes}
+                    cartGood={cartGood}
+                    dispatch={dispatch}
+                />
             ))}
         </div>
-    )
-}
+    );
+};
 
-export default CartItemsList
+export default CartItemsList;
